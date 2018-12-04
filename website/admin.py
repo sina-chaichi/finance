0d158153fd074a74f3ec688597bb703fdf9be0c7
sina <sina.chaichi2020@gmail.com>
2018-12-04 13:05:26 +0330

    income in models

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from website.models import Expense
+from website.models import Income
+
 
 # Register your models here.
 admin.site.register(Expense)
+admin.site.register(Income)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -8,3 +9,14 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return '{}-{}-{}'.format(self.text,self.date,self.amount)
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return '{}-{}-{}'.format(self.text,self.date,self.amount)
